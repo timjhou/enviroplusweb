2dfe8b055adc4eba11294be9a2a9f5164cf0ac21
i.j <info@idotj.com>
2022-03-31 17:27:32 +0000

    config by default

--- enviroplusweb.py
+++ enviroplusweb.py
@@ -12,11 +12,11 @@
 #
 
 # If you prefer to keep the Enviro LCD screen off, change the next value to False
-lcd_screen = False
+lcd_screen = True
 # If you don't have a fan plugged on GPIO, change the next value to False
-fan_gpio = False
+fan_gpio = True
 # Temperature and humidity compensation (edit values 'factor_temp' and 'factor_humi' to adjust them)
-temp_humi_compensation = True
+temp_humi_compensation = False
 # If you have an Enviro board without gas sensor, change the next value to False
 gas_sensor = True
 # If you don't have a particle sensor PMS5003 attached, change the next value to False
@@ -334,7 +334,7 @@ if __name__ == '__main__':
         days.append(read_day('enviro-data/' + f))
     background_thread.start()
     try:
-        app.run(debug = False, host = '0.0.0.0', port = 81, use_reloader = False)
+        app.run(debug = False, host = '0.0.0.0', port = 80, use_reloader = False)
     except Exception as e:
         print(e)
         pass
